e90e037a7403bb7fa6863f0e856ea7a1f08274d4
Robert Bryan <rbryan@elevatedthird.com>
2025-11-03 11:31:02 -0500

    update tooltip error catching, component yml files

--- src/01-atom/sq-tooltip/sq-tooltip.es6.js
+++ src/01-atom/sq-tooltip/sq-tooltip.es6.js
@@ -11,13 +11,24 @@ import { Tooltip } from 'foundation-sites/js/foundation.tooltip';
     attach(context) {
       once('ariesTooltip', '.has-tip').forEach((tooltip) => {
         // Find the closest parent element that has background color set.
-        var bgClass = $(tooltip).closest("[class*='bg-color-']").attr('class').split(' ')
-          .find((c) => c.startsWith('bg-color-'));
-        if (!bgClass) {
-          bgClass = $(tooltip).closest("[class*='sub-bg-color-']").attr('class').split(' ')
-            .find((c) => c.startsWith('sub-bg-color-'));
-          bgClass = bgClass.replace('sub-', '');
+        var bgClassDiv = $(tooltip).closest("[class*='bg-color-']");
+        var bgClass = '';
+
+        if (bgClassDiv.length) {
+          bgClass = bgClassDiv.attr('class').split(' ')
+            .find((c) => c.startsWith('bg-color-'));
         }
+
+        if (!bgClass) {
+          var subBgClassDiv = $(tooltip).closest("[class*='sub-bg-color-']");
+
+          if (subBgClassDiv.length) {
+            bgClass = subBgClassDiv.attr('class').split(' ')
+              .find((c) => c.startsWith('sub-bg-color-'));
+            bgClass = bgClass.replace('sub-', '');
+          }
+        }
+
         const _tooltip = new Tooltip($(tooltip), {
           allowHtml: true,
           hoverDelay: 150,
