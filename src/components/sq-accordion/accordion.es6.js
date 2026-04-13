59e0899df35cff7df6a2911b05b53abf08a91de9
MattLucasiewicz <matt.lucasiewicz@transamerica.com>
2026-04-13 16:44:20 -0400

    latest.

--- src/components/sq-accordion/accordion.es6.js
+++ src/components/sq-accordion/accordion.es6.js
@@ -5,19 +5,20 @@
 
 import { Accordion } from 'foundation-sites/js/foundation.accordion';
 
+(function ($, Drupal, once) {
+  Drupal.behaviors.ariesAccordion = {
+    attach(context) {
+      once('ariesAccordion', context.querySelectorAll('.accordion:not(.organism--sq-disclaimer .accordion)')).forEach((accordion) => {
+        const accordionEl = new Accordion($(accordion), { multiExpand: true });
 
-Drupal.behaviors.ariesAccordion = {
-  attach(context) {
-    once('ariesAccordion', '.accordion:not(.organism--sq-disclaimer .accordion)').forEach((accordion) => {
-      const accordionEl = new Accordion($(accordion), { multiExpand: true });
-
-      // @todo i think this can be removed.
-      if ($(accordion).closest('.view-financial-results').length > 0) {
-        const firstContent = $(accordion).find('.accordion-content').first();
-        if (firstContent.length > 0) {
-          accordionEl.toggle(firstContent);
+        // @todo i think this can be removed.
+        if ($(accordion).closest('.view-financial-results').length > 0) {
+          const firstContent = $(accordion).find('.accordion-content').first();
+          if (firstContent.length > 0) {
+            accordionEl.toggle(firstContent);
+          }
         }
-      }
-    });
-  },
-};
+      });
+    },
+  };
+})(jQuery, Drupal, once);
